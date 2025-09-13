b0486f47e7900ca9e7e73ab253d6a4105d2b8f56
Atul Joshi <144705772+atuljoshii@users.noreply.github.com>
2025-09-13 13:49:03 +0530

    ok

---
+++ Swap.py
@@ -0,0 +1,14 @@
+print("Swap Two Numbers Without Using third veriable")
+
+a= 20
+b= 10
+
+print("Before Swaping Veriable A is = ",a)
+print("Before Swaping Veriable B is = ",b)
+
+a = a+b;  
+b = a-b;  
+a = a-b;
+
+print("After Swaping Veriable A is = ",a)
+print("After Swaping Veriable B is = ",b)
---
+++ SwapThird.py
@@ -0,0 +1,15 @@
+print("Swap Two Numbers With Using third veriable")
+
+a= 20
+b= 10
+
+print("Before Swaping Veriable A is = ",a)
+print("Before Swaping Veriable B is = ",b)
+
+c=a
+a=b
+b=c
+
+print("After Swaping Veriable A is = ",a)
+print("After Swaping Veriable B is = ",b)
+print(c)
